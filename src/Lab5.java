d99aab1e96560c099c46e1dc07a06e3b3d7bb689
Nabil M. Al-Rousan <rousani@gmail.com>
2022-10-06 10:25:05 -0700

    Task 2 - fail fast
    using IllegalArgumentException

--- src/Lab5.java
+++ src/Lab5.java
@@ -1,14 +1,17 @@
 public class Lab5 {
     public static void main(String[] args) {
-        System.out.println(dayOfYear(6, 22, 2022)); // 173
-        System.out.println(dayOfYear(10, 6, 2022)); // 279
+        System.out.println(dayOfYear(26, 22, 2022)); // should fail fast
     }
 
     private static int dayOfYear(int month, int dayOfMonth, int year) {
-        int[] monthLengths = new int[] { 31, 28, 31, 30, 31, 30, 31 , 31, 30 , 31, 30,  31 };
+        // fail fast -todo
+        if (month < 1 || month > 12)
+            throw new IllegalArgumentException();
+
+        int[] monthLengths = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         int sum = 0;
-        for(int i= 1; i < month ; i++){
-            sum += monthLengths[i-1];
+        for (int i = 1; i < month; i++) {
+            sum += monthLengths[i - 1];
         }
         sum += dayOfMonth;
         return sum;
