547d93d813643531ba449f2961243e54344810d6
Nabil M. Al-Rousan <rousani@gmail.com>
2022-10-06 10:35:58 -0700

    Task 2 - Fail fast
    using enum

--- src/Lab5.java
+++ src/Lab5.java
@@ -1,16 +1,15 @@
 public class Lab5 {
     public static void main(String[] args) {
-        System.out.println(dayOfYear(26, 22, 2022)); // should fail fast
+        System.out.println(dayOfYear(Month.JUNE, 22, 2022)); // should fail fast
+        System.out.println(Month.JUNE.ordinal() + 1);
     }
-
-    private static int dayOfYear(int month, int dayOfMonth, int year) {
-        // fail fast -todo
-        if (month < 1 || month > 12)
-            throw new IllegalArgumentException();
+    public static enum Month { JANUARY, FEBRUARY, MARCH, APRIL, MAY, JUNE, JULY, AUGUST,
+        SEPTEMBER, OCTOBER, NOVEMBER, DECEMBER}
+    private static int dayOfYear(Month month, int dayOfMonth, int year) {
 
         int[] monthLengths = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
         int sum = 0;
-        for (int i = 1; i < month; i++) {
+        for (int i = 1; i < month.ordinal() + 1; i++) {
             sum += monthLengths[i - 1];
         }
         sum += dayOfMonth;
