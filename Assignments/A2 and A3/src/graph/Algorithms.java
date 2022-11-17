896901ad37b21dfde140426b584f2bd7493c0d1a
Nabil M. Al-Rousan <rousani@gmail.com>
2022-11-17 10:14:05 -0800

    ..

--- Assignments/A2 and A3/src/graph/Algorithms.java
+++ Assignments/A2 and A3/src/graph/Algorithms.java
@@ -1,31 +1,30 @@
-package ca.ubc.ece.cpen221.mp3.graph;
 
-import ca.ubc.ece.cpen221.mp3.staff.Graph;
-import ca.ubc.ece.cpen221.mp3.staff.Vertex;
+import staff.Graph;
+import staff.Vertex;
 
 public class Algorithms {
 
-	/**
-	 * *********************** Algorithms ****************************
-	 * 
-	 * Please see the README for the machine problem for a more detailed
-	 * specification of the behavior of each method that one should implement.
-	 */
+  /**
+   * *********************** Algorithms ****************************
+   * 
+   * Please see the README for the machine problem for a more detailed
+   * specification of the behavior of each method that one should implement.
+   */
 
-	/**
-	 * This is provided as an example to indicate that this method and other
-	 * methods should be implemented here.
-	 * 
-	 * You should write the specs for this and all other methods.
-	 * 
-	 * @param graph
-	 * @param a
-	 * @param b
-	 * @return
-	 */
-	public static int shortestDistance(Graph graph, Vertex a, Vertex b) {
-		// TODO: Implement this method and others
-		return 0;
-	}
+  /**
+   * This is provided as an example to indicate that this method and other
+   * methods should be implemented here.
+   * 
+   * You should write the specs for this and all other methods.
+   * 
+   * @param graph
+   * @param a
+   * @param b
+   * @return
+   */
+  public static int shortestDistance(Graph graph, Vertex a, Vertex b) {
+    // TODO: Implement this method and others
+    return 0;
+  }
 
 }
